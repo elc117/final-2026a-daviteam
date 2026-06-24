c514c3e272806ef724c7e42b975cd6a74f337233
Davi Libardoni <davijaum@gmail.com>
2026-06-23 21:43:00 -0300

    Removi código que não coube no escopo da implementação, finalizei a lógica do review das cartas + correção de um bug no index.html

--- src/main/java/service/CardService.java
+++ src/main/java/service/CardService.java
@@ -16,20 +16,51 @@ public class CardService {
 			deck.incrementReviewed();
 		}
 		
+		int newEase, interval;
+		int ease = card.getEase();
+		int success = card.getSuccessfulReviews();
+		float easeRate = (float) (card.getEase() / 100.0f);
 		switch(rating) {
 		case MISS:
 			card.setNextReview(LocalDate.now().plusDays(1));
+			card.setLastInterval(1);
 			card.setSuccessfulReviews(0);
-			int newEase = card.getEase();
+			
+			newEase = Math.max(ease - 50, 130);
 			card.setEase(newEase);
 			break;
+			
 		case HARD:
-			break;
-		case EASY:
-			break;
-		default:
+			if(success == 0) { 
+				interval = 1;
+			}else if(success == 1) { 
+				interval = 3;
+			}else {
+				interval = Math.round(card.getLastInterval() * easeRate);
+			}
+			card.setLastInterval(interval);
+			card.setNextReview(LocalDate.now().plusDays(interval));
+			card.setSuccessfulReviews(card.getSuccessfulReviews()+1);
+			
+			newEase = Math.max(ease - 15, 130);
+			card.setEase(newEase);
 			break;
 			
+		case EASY:
+			if(success == 0) { 
+				interval = 1;
+			}else if(success == 1) { 
+				interval = 6;
+			}else {
+				interval = Math.round(card.getLastInterval() * easeRate);
+			}
+			card.setLastInterval(interval);
+			card.setNextReview(LocalDate.now().plusDays(interval));
+			card.setSuccessfulReviews(card.getSuccessfulReviews()+1);
+			
+			newEase = Math.min(ease + 15, 500);
+			card.setEase(newEase);
+			break;
 		}
 	}
 }
